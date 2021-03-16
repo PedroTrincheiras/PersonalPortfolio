cbe32a39b20541e1ba53aaea3952ec9143a22208
PedroTrincheiras <59057189+PedroTrincheiras@users.noreply.github.com>
2021-03-16 21:15:13 +0000

    Fix CSS Glitch #3

--- components/header/header.js
+++ components/header/header.js
@@ -1,58 +1,90 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
 import HamburgerMenu from "react-hamburger-menu";
-import React from "react";
+import React, { useState} from "react";
+import { motion} from "framer-motion";
 
-class Header extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { open: false };
-  }
+function Header() {
+  const [open, toggle] = useState(false);
 
-  handleClick() {
-    this.setState({
-      open: !this.state.open,
-    });
-  }
-  render() {
-    return (
-      <div>
-        <header>
-          <div className="left">
-            <a href="./">Pedro Trincheiras</a>
-          </div>
-          <div className="right">
-            <a className="icon" href="https://github.com/PedroTrincheiras" target="_blank">
-              <FontAwesomeIcon icon={faGithub} />
-            </a>
-            <a className="icon" href="https://www.linkedin.com/in/pedrotrincheiras" target="_blank">
-              <FontAwesomeIcon icon={faLinkedin} />
-            </a>
-            <HamburgerMenu
-              className="hmenu"
-              isOpen={this.state.open}
-              menuClicked={this.handleClick.bind(this)}
-              width={30}
-              height={30}
-              strokeWidth={3}
-              rotate={0}
-              color="white"
-              borderRadius={50}
-              animationDuration={0.5}
-            />
-          </div>
-        </header>
-        <spam className={this.state.open ? "dropmenu active" : "dropmenu"}>
-          <nav>
-            <a className={this.state.open ? "active" : ""} href = "#projects" onClick={this.handleClick.bind(this)}>PROJECTS</a>
-            <a className={this.state.open ? "active" : ""} href = "#certifications" onClick={this.handleClick.bind(this)}>CERTIFICATIONS</a>
-            <a className={this.state.open ? "active" : ""} href = "#skills" onClick={this.handleClick.bind(this)}>SKILLS</a>
-            <a className={this.state.open ? "active" : ""} href = "#contacts" onClick={this.handleClick.bind(this)}>CONTACTS</a>
-          </nav>
-        </spam>
-      </div>
-    );
-  }
+  const fade = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1 },
+  };
+
+  return (
+    <div>
+      <header>
+        <div className="left">
+          <a href="./">Pedro Trincheiras</a>
+        </div>
+        <div className="right">
+          <a
+            className="icon"
+            href="https://github.com/PedroTrincheiras"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+          <a
+            className="icon"
+            href="https://www.linkedin.com/in/pedrotrincheiras"
+            target="_blank"
+          >
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <HamburgerMenu
+            className="hmenu"
+            isOpen={open}
+            menuClicked={() => toggle(!open)}
+            width={30}
+            height={30}
+            strokeWidth={3}
+            rotate={0}
+            color="white"
+            borderRadius={50}
+            animationDuration={0.5}
+          />
+        </div>
+      </header>
+      <motion.spam
+        className={open ? "dropmenu active" : "dropmenu"}
+        variants={fade}
+        transition={{ duration: 3 }}
+      >
+        <nav>
+          <a
+            className={open ? "active" : ""}
+            href="#projects"
+            onClick={() => toggle(!open)}
+          >
+            PROJECTS
+          </a>
+          <a
+            className={open ? "active" : ""}
+            href="#certifications"
+            onClick={() => toggle(!open)}
+          >
+            CERTIFICATIONS
+          </a>
+          <a
+            className={open ? "active" : ""}
+            href="#skills"
+            onClick={() => toggle(!open)}
+          >
+            SKILLS
+          </a>
+          <a
+            className={open ? "active" : ""}
+            href="#contacts"
+            onClick={() => toggle(!open)}
+          >
+            CONTACTS
+          </a>
+        </nav>
+      </motion.spam>
+    </div>
+  );
 }
 
 export default Header;
