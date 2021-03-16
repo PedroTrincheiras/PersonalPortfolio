e0e3d097803731ebb1eb2782277e8fd4b9aafadf
PedroTrincheiras <59057189+PedroTrincheiras@users.noreply.github.com>
2021-03-16 20:58:51 +0000

    Fix CSS Glitch #2

--- components/footer/footer.js
+++ components/footer/footer.js
@@ -1,12 +1,41 @@
 import React from "react";
+import { motion, useAnimation } from "framer-motion";
+import { useInView } from "react-intersection-observer";
+import { useEffect } from "react";
 
 function Footer() {
   var d = new Date().getFullYear();
+  const fadeBottom = {
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, y: [1000, -0, 0] },
+  };
+
+  const controls = useAnimation();
+  const [ref, inView] = useInView();
+
+  useEffect(() => {
+    if (inView) {
+      controls.start("visible");
+    } else {
+      controls.start("hidden");
+    }
+  }, [controls, inView]);
+
   return (
-    <footer>
-      <p>Made by Pedro Trincheiras<span>.</span></p>
-      <small>Copyright &copy; {d}, Pedro Trincheiras. All rights reserved.</small>
-    </footer>
+    <motion.footer
+      ref={ref}
+      animate={controls}
+      variants={fadeBottom}
+      initial="hidden"
+      transition={{ duration: 1 }}
+    >
+      <p>
+        Made with <span>❤️</span> by Pedro Trincheiras.
+      </p>
+      <small>
+        Copyright &copy; {d}, Pedro Trincheiras. All rights reserved.
+      </small>
+    </motion.footer>
   );
 }
 
