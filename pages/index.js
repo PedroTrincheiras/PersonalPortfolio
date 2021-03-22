931f5884557fdd24dedc8aa8cdaf67b9c6a0f914
PedroTrincheiras <59057189+PedroTrincheiras@users.noreply.github.com>
2021-03-22 14:17:55 +0000

    Google description

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,7 @@ function Home({ data }) {
       <Head>
         <title>Pedro Trincheiras</title>
         <link rel="icon" href="/favicon.ico" />
+        <meta name="description" content="Hello. I'm Pedro. I'm a Computer Engineering Student and this is my website."/>
       </Head>
       <Header />
       <ParticleBG />
